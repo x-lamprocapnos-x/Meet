bc7197293e7b48c249bb43544a2c673dc3df11e9
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-07-05 19:45:20 -0400

    second commit

---
+++ auth-server/handler.js
@@ -0,0 +1,90 @@
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar("v3");
+
+/* SCOPES allows you to set access levels; this is set to read only for now because you don't have access rights to update the calendar yourself. */
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+/* Credentials are values required to get access to your calendar. If you see “process.env” this means the value is in the “config.json” file. */
+
+const credentials = {
+  client_id: process.env.CLIENT_ID,
+  project_id: process.env.PROJECT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris: ["https://x-lamprocapnos-x.github.io/meet"],
+  javascript_origins: ["https://x-lamprocapnos-x.github.io", "http://localhost:3000"],
+};
+
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
+
+module.exports.getAuthUrl = async () => {
+
+  console.log("authUrl");
+
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      authUrl: authUrl,
+    }),
+  };
+};
+
+module.exports.getAccessToken = async (event) => {
+
+  console.log("getAccessToken");
+
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(token);
+    });
+  })
+    .then((token) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(token),
+      };
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
+};
