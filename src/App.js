a11f31df7094ac41770ae7b2a37570f1c508dc96
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-07-17 20:58:15 -0400

    - Import EventList - Add <div> - add Id attribute - add <eventList/>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <div id="event-list">
+        <EventList />
+      </div>
       
     </div>
   );
