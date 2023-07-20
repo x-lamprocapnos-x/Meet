a9a2479e6f96adf8a7c745b28d0fe58445329307
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-07-20 17:45:38 -0400

    - Create CitySearch.test.js - Add content

---
+++ src/__tests__/CitySearch.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable testing-library/no-render-in-setup */
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable testing-library/render-result-naming-convention */
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CitySearch from '../components/CitySearch';
+
+describe('<CitySearch /> component', () => {
+    let CitySearchComponent;
+    beforeEach(() => {
+        CitySearchComponent = render(<CitySearch />);
+    });
+
+    test('renders text input', () => {
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        expect(cityTextBox).toBeInTheDocument();
+        expect(cityTextBox).toHaveClass('city');
+    });
+
+    test('suggestions list is hidden by default', () => {
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).not.toBeInTheDocument();
+      });
+    
+      test('renders a list of suggestions when city textbox gains focus', async () => {
+        const user = userEvent.setup();
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.click(cityTextBox);
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).toBeInTheDocument();
+        expect(suggestionList).toHaveClass('suggestions');
+      });
+
+});
