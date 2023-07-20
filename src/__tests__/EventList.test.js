a09f36c8ed6ccabcb0862bce2b095270bb090528
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-07-20 17:45:11 -0400

    format and reorganize code, eslint-disable

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,17 +1,25 @@
+/* eslint-disable testing-library/no-render-in-setup */
+/* eslint-disable testing-library/prefer-presence-queries */
+/* eslint-disable testing-library/render-result-naming-convention */
+/* eslint-disable testing-library/prefer-screen-queries */
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
 import Event from '../components/Event';
 
 describe('<EventList /> component', () => {
-  test('has an element with "list" role', () => {
-    const EventListComponent = render(<EventList />);
-    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
-  });
+    let EventListComponent;
+    beforeEach(() => {
+        EventListComponent = render(<EventList />);
+    })
 
-  test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={
-        [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
-    }/>);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
-  });
+    test('has an element with "list" role', () => {
+        expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+    });
+
+    test('renders correct number of events', () => {
+        EventListComponent.rerender(<EventList events={
+            [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+        } />);
+        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+    });
 });
