7147ca2e85709fda5bd8b4a333ef4bdd1553d181
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-08-09 20:42:52 -0400

    remove eslint comments

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,3 @@
-/* eslint-disable testing-library/no-render-in-setup */
-/* eslint-disable testing-library/no-node-access */
 import { render } from '@testing-library/react';
 import App from '../App';
 
