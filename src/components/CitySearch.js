ab22fb0a954dd23051080ef79a777b327c3c7d79
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-09-05 20:37:24 -0400

    - update CitySearch.js to add functionality

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
-
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
     const [showSuggestions, setShowSuggestions] = useState(false);
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+      setSuggestions(allLocations);
+    }, [`${allLocations}`]);
     
     const handleInputChanged = (event) => {
         const value = event.target.value;
@@ -21,14 +24,15 @@ const CitySearch = ({ allLocations }) => {
         const value = event.target.textContent;
         setQuery(value);
         setShowSuggestions(false); // to hide the list
+        setCurrentCity(value);
       };
 
       return (
         <div id="city-search">
           <input
-            type="text"
             className="city"
             placeholder="Search for a city"
+            type="text"
             value={query}
             onFocus={() => setShowSuggestions(true)}
             onChange={handleInputChanged}
