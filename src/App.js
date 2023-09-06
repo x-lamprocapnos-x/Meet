2a3a3fe037447bf4550f4888d742fe4d0564a532
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-09-05 20:38:44 -0400

    - add functionality and component interlace

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
+import { useState, useEffect } from 'react';
+import { extractLocations, getEvents } from './api';
+
 import './App.css';
 
 const App = () => {
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [events, setEvents] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+   useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+        allEvents :
+        allEvents.filter(event => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
   return (
     <div className="App">
       <div id="event-list">
-        <CitySearch />
-        <EventList />
+        <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+        <NumberOfEvents />
+        <EventList events={events} />
       </div>
-      
+
     </div>
   );
 }
