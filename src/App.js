f1ff061c002cf2a0bdc08c5ca47e03dfdf71e051
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-10-10 18:27:17 -0400

    add infoAlert to app to trigger from citySearch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useState, useEffect } from 'react';
 import { extractLocations, getEvents } from './api';
+import { InfoAlert } from './components/Alert';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -28,8 +30,15 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+      </div>
       <div id="event-list">
-        <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+        <CitySearch 
+        allLocations={allLocations} 
+        setCurrentCity={setCurrentCity} 
+        setInfoAlert={setInfoAlert}
+        />
         <NumberOfEvents setCurrentNOE={setCurrentNOE} />
         <EventList events={events} />
       </div>
