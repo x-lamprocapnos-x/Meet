6fdc2c697b533f585cc8e3703bb5d970097bbef7
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-10-21 17:28:23 -0400

    add WarningAlert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -44,9 +44,17 @@ class InfoAlert extends Alert {
 class ErrorAlert extends Alert {
     constructor(props) {
         super(props);
-        this.color = 'rgb (255, 0, 0)'; // red
+        this.color = 'rgb(255, 0, 0)'; // red
         this.bgColor = 'rgb(255, 220, 220)'; // light red
     }
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'rgb(255, 255, 0)'; // yellow
+        this.bgColor = 'rgb(255, 255, 220)'; // light yellow
+    }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
